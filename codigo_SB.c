a1062cbab5c9ae0b17a5f228b8b1a75e74809947
Iago Floriano <iagomfloriano@gmail.com>
2022-04-21 15:44:26 -0300

    Adicionada cast de tipos para compilar, e adicionado printf em inicia alocador

--- codigo_SB.c
+++ codigo_SB.c
@@ -1,71 +1,77 @@
 #include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
 
 void* atual_p;
 void* inicio_heap;
+void* fim_heap;
 
 void *alocaMem (int num_bytes){
   void*  ponteiro_mem = atual_p;
-  int    tam          = *(ponteiro_mem - 8);
-  int    estado       = *(ponteiro_mem - 16);
+  int64_t tam         = *(int64_t *)(ponteiro_mem - 8);
+  int64_t estado      = *(int64_t *)(ponteiro_mem - 16);
   
   while (!( (tam >= num_bytes) && (estado == 0) )){
     ponteiro_mem += tam + 16;
     if (ponteiro_mem == atual_p){
       //o ponteiro deu a volta inteira e nao encontrou espaço
-      while (ponteiro_mem < brk){
+      while (ponteiro_mem < fim_heap){
         //enquanto o ponteiro estiver dentro da heap
         ponteiro_mem += tam + 16;
-        tam = *(ponteiro_mem - 8);
+        tam = *(int64_t *)(ponteiro_mem - 8);
       }
       
-      brk += (1024 + 16);
+      fim_heap += (1024 + 16);
+      sbrk((int64_t)fim_heap);
       //aumenta a brk de alguma forma para aumentar a heap 
       
-      *(ponteiro_mem - 8)  = 1024;
-      *(ponteiro_mem - 16) = 0;
+      *(int64_t *)(ponteiro_mem - 8)  = (int8_t)1024;
+      *(int64_t *)(ponteiro_mem - 16) = (int8_t)0;
       //adiciona um espaço livre de 1024
     }
     
-    if (ponteiro_mem > brk)
-        ponteiro_mem = inicio_brk + 16;
+    if (ponteiro_mem > fim_heap)
+        ponteiro_mem = inicio_heap + 16;
     //dependendo do jeito de pegar o inicio_brk, talvez nao precise de +16
     
-    tam = *(ponteiro_mem - 8);
-    estado = *(ponteiro_mem - 16);
+    tam = *(int64_t *)(ponteiro_mem - 8);
+    estado = *(int64_t *)(ponteiro_mem - 16);
   }
 
-  *(ponteiro_mem - 16) = 1;
+  *(int64_t *)(ponteiro_mem - 16) = 1;
   if (!( tam > num_bytes + 16)) { //caso dê para criar um novo bloco em tam - num_bytes
-    *(ponteiro_mem + num_bytes)     = 0;                    //estado do próximo bloco como livre
-    *(ponteiro_mem + num_bytes + 8) = tam - num_bytes - 16; //tamanho do próximo bloco como tamanho antigo desse bloco menos info
-    *(ponteiro_mem - 8)             = num_bytes;            //salva tamanho desse bloco
+    *(int64_t *)(ponteiro_mem + num_bytes)     = 0;                    //estado do próximo bloco como livre
+    *(int64_t *)(ponteiro_mem + num_bytes + 8) = tam - num_bytes - 16; //tamanho do próximo bloco como tamanho antigo desse bloco menos info
+    *(int64_t *)(ponteiro_mem - 8)             = num_bytes;            //salva tamanho desse bloco
   }
   return ponteiro_mem;
 }
 
-void iniciaAlocador {
+void iniciaAlocador() {
+  printf("");
   inicio_heap = sbrk(0);
+  fim_heap = sbrk(0);
   //chama a syscall que retorna o valor atual de brk
 
   atual_p      = inicio_heap;
 }
 
 void finalizaAlocador (){
-  brk(inicio_heap);
+  sbrk((int64_t)inicio_heap);
   //chama a syscall alterando o valor da brk pro encontrado no iniciaAlocador
 }
 
 void liberaAlocador (void* bloco){
   void* est_end = bloco - 16;
-  *(est_end) = 0;
+  *(int64_t *)(est_end) = 0;
   //pega o endereço do estado do bloco, coloca "vazio" nele
 
   //terminar de programar a junção de blocos vazios
 
-  int   tam     = *(bloco - 8)
-  void* end_aux = bloco + tam;
-  if (*(end_aux) == 0){   //juntar o bloco da frente
-    *(bloco-8) += *(end_aux+8) + 16;  //o tamanho do bloco vai virar o tamanho atual + tamanho do da frente + 16
+  int64_t tam     = *(int64_t *)(bloco - 8);
+  void*   end_aux = bloco + tam;
+  if (*(int64_t *)(end_aux) == 0){   //juntar o bloco da frente
+    *(int64_t *)(bloco-8) += *(int64_t *)(end_aux+8) + 16;  //o tamanho do bloco vai virar o tamanho atual + tamanho do da frente + 16
   }
 
 
