eae000a0a0eb57006199ea9b6edd24c041bde851
JoaoLCordeiro <jlc19@inf.ufpr.br>
2022-04-21 15:05:46 -0300

    nao testei ainda mas acho que funciona agora, tenho que fazer uma main pra testar

--- codigo_SB.c
+++ codigo_SB.c
@@ -12,21 +12,21 @@ void *alocaMem (int num_bytes){
     ponteiro_mem += tam + 16;
     if (ponteiro_mem == atual_p){
       //o ponteiro deu a volta inteira e nao encontrou espaço
-      while (ponteiro_mem < brk){
+      while (ponteiro_mem < sbrk(0)){
         //enquanto o ponteiro estiver dentro da heap
         ponteiro_mem += tam + 16;
         tam = *(ponteiro_mem - 8);
       }
       
-      brk += (1024 + 16);
-      //aumenta a brk de alguma forma para aumentar a heap 
+      sbrk (1024 + 16);
+      //aumenta a brk em 2014
       
       *(ponteiro_mem - 8)  = 1024;
       *(ponteiro_mem - 16) = 0;
       //adiciona um espaço livre de 1024
     }
     
-    if (ponteiro_mem > brk)
+    if (ponteiro_mem > sbrk(0))
         ponteiro_mem = inicio_brk + 16;
     //dependendo do jeito de pegar o inicio_brk, talvez nao precise de +16
     
@@ -64,9 +64,24 @@ void liberaAlocador (void* bloco){
 
   int   tam     = *(bloco - 8)
   void* end_aux = bloco + tam;
-  if (*(end_aux) == 0){   //juntar o bloco da frente
+  if (*(end_aux) == 0){               //juntar o bloco da frente
     *(bloco-8) += *(end_aux+8) + 16;  //o tamanho do bloco vai virar o tamanho atual + tamanho do da frente + 16
   }
 
+  tam     = *(bloco - 8);
+  end_aux = bloco + tam;
 
+  void* fim_heap = sbrk(0);
+
+  while (end_aux + *(end_aux - 8) != bloco){    //chega no bloco anterior
+    tam     = *(end_aux - 8);
+    end_aux += tam;
+
+    if (end_aux >= fim_heap)                    //chegando no final, vai pro começo
+      end_aux = inicio_heap + 16;
+  }
+
+  if (*(end_aux) == 0){               //juntar o bloco de trás
+    *(end_aux-8) += *(bloco-8) + 16;  //o tamanho do bloco vai virar o tamanho do atual + o do que foi liberto
+  }
 }
